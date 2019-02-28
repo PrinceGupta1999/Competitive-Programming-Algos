0a305bdb5d5876618397133d294d69ff6dc463f4
PrinceGupta1999 <32774830+PrinceGupta1999@users.noreply.github.com>
2019-03-01 01:40:51 +0530

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#define mod 1000000007
+typedef long long ll;
+typedef long double ld;
+template <typename T> using indexed_set = tree<T, null_type, less<>, rb_tree_tag, tree_order_statistics_node_update>;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t, n, i;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+
+    }
+    return 0;
+}
