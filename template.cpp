2cb6bcb04266eeb157b7c0e3e4c2db8932449f3b
PrinceGupta1999 <32774830+PrinceGupta1999@users.noreply.github.com>
2019-03-01 01:47:46 +0530

    Added Comments

--- template.cpp
+++ template.cpp
@@ -2,16 +2,21 @@
 #include <ext/pb_ds/assoc_container.hpp>
 using namespace __gnu_pbds;
 using namespace std;
-#define mod 1000000007 
+#define mod 1000000007 // common value of MOD on codechef
 typedef long long ll;
 typedef long double ld;
 template <typename T> using indexed_set = tree<T, null_type, less<>, rb_tree_tag, tree_order_statistics_node_update>; // an improved RB Tree for getting node <---> order
 int main()
 {
-    /* fast input and output */
+    /* Reading from and Writing to Files 
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    */
+    /* fast input and output
     ios::sync_with_stdio(false); 
     cin.tie(nullptr);
     cout.tie(nullptr);
+     */
     ll t, n, i;
     cin>>t;
     while(t--)
@@ -19,5 +24,9 @@ int main()
         cin>>n;
         cout<<t<<" "<<n<<"\n";
     }
+    /* Closing STDIN and STDOUT files
+    fclose(stdin);
+    fclose(stdout);
+    */
     return 0;
 }
