207033b33f849861621daf4b5afdf344ecdfd55f
Prince Gupta <guptaprince223@gmail.com>
2019-04-12 23:34:18 +0530

    Added Comments

--- Huffman Codes.cpp
+++ Huffman Codes.cpp
@@ -3,12 +3,12 @@ using namespace std;
 struct Node
 {
     Node *left, *right;
-    int frequency;
-    char character;
+    int frequency; // Frequency of Character(s)
+    char character; // Character represented by leaf node otherwise '*'
 };
 struct compareNodes
 {
-    bool operator()(Node *first, Node *other)
+    bool operator()(Node *first, Node *other) // Custom compare func. to use in heap
     {
         return first->frequency > other->frequency;
     }
@@ -18,7 +18,7 @@ int main()
     priority_queue<Node*, vector<Node*>, compareNodes> heap;
     int n, i;
     cin>>n;
-    for(i = 0; i < n; i++)
+    for(i = 0; i < n; i++) // Iniitalizations
     {
         auto z = new Node;
         z->left = z->right = nullptr;
@@ -27,19 +27,19 @@ int main()
     }
     for(i = 0; i < n - 1; i++)
     {
-        auto z = new Node;
-        z->character = '*';
+        auto z = new Node; // Create New Intermediate Node
+        z->character = '*'; // Initialize to Default Values
         z->frequency = 0;
         z->left = heap.top();
+        z->frequency += heap.top()->frequency; // Get Smallest Nodes 
+        heap.pop();
+        z->right = heap.top(); 
         z->frequency += heap.top()->frequency;
         heap.pop();
-        z->right = heap.top();
-        z->frequency += heap.top()->frequency;
-        heap.pop();
-        heap.push(z);
+        heap.push(z); // Push the new Intermediate Node
     }
     auto z = heap.top();
     heap.pop();
-    cout<<z->frequency;
+    cout<<z->frequency; // Just O/P to verify result. Can fetch all codes for characters.
     return 0;
 }
